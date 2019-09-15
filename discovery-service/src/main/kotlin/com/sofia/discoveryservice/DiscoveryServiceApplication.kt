a4d50015da7167ef763fb0f43ae1c1e6adbfac97
Sofia Lunkes <sofia.big@live.com>
2019-09-15 00:33:09 -0300

    added eureka server and basic configuration to bootstrap

--- discovery-service/src/main/kotlin/com/sofia/discoveryservice/DiscoveryServiceApplication.kt
+++ discovery-service/src/main/kotlin/com/sofia/discoveryservice/DiscoveryServiceApplication.kt
@@ -2,8 +2,10 @@ package com.sofia.discoveryservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryServiceApplication
 
 fun main(args: Array<String>) {
